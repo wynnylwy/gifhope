0debc6faf09aa5f97c8976f4a34b32fbbb709459
Wynn <wynnylimweiyi@gmail.com>
2021-03-09 17:37:00 +0800

    Add: DropDown + RM

--- lib/report_donate.dart
+++ lib/report_donate.dart
@@ -16,64 +16,118 @@ class DonateReportScreen extends StatefulWidget {
 class _DonateReportScreenState extends State<DonateReportScreen> {
   List<charts.Series<CDonate, String>> seriesBarData;
 
-  @override
+  List<String> listMonth = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
 
+  String selectedMonth;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-            iconTheme: IconThemeData(color: Colors.white),
-            flexibleSpace: Container(
-              decoration: new BoxDecoration(
-                gradient: new LinearGradient(
-                    colors: [
-                      const Color(0xFFFDD835),
-                      const Color(0xFFFBC02D),
-                    ],
-                    begin: const FractionalOffset(0.0, 0.0),
-                    end: const FractionalOffset(1.0, 0.0),
-                    stops: [0.0, 1.0],
-                    tileMode: TileMode.clamp),
+        iconTheme: IconThemeData(color: Colors.black),
+        flexibleSpace: Container(
+          decoration: new BoxDecoration(
+            gradient: new LinearGradient(
+                colors: [
+                  const Color(0xFFFDD835),
+                  const Color(0xFFFBC02D),
+                ],
+                begin: const FractionalOffset(0.0, 0.0),
+                end: const FractionalOffset(1.0, 0.0),
+                stops: [0.0, 1.0],
+                tileMode: TileMode.clamp),
+          ),
+        ),
+        title: Text('Donation Report',
+            style: TextStyle(
+                fontFamily: 'Sofia',
+                fontWeight: FontWeight.bold,
+                fontSize: 30.0,
+                color: Colors.black)),
+      ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start, 
+          children: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(150, 10, 5, 50),
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.05,
+              child: Row(
+                children: [
+                  Text("Month Selected: "),
+                  SizedBox(width: 28),
+                  DropdownButton(
+                      value: selectedMonth,
+                      items: listMonth.map((selectedMonth) {
+                        return DropdownMenuItem(
+                            child: new Text(
+                              selectedMonth,
+                              textAlign: TextAlign.center,
+                            ),
+                            value: selectedMonth);
+                      }).toList(),
+                      onChanged: (newValue) {
+                        setState(() {
+                          selectedMonth = newValue;
+                          print(selectedMonth);
+                        });
+                      }),
+                ],
               ),
             ),
-            title: Text('Donation Report',
-                style: TextStyle(
-                    fontFamily: 'Sofia',
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30.0,
-                    color: Colors.white)),
           ),
-      body: Center(
-        child: Container(
-          height: MediaQuery.of(context).size.height * 0.40,
-          child: FutureBuilder(
-              future: getData(),
-              builder: (BuildContext context,AsyncSnapshot snapshot) {
-                if (snapshot.connectionState == ConnectionState.none || snapshot.hasData == false)
-                {
-                  return Center(
-                    child: CircularProgressIndicator()
-                  );
-                }
-
-                else 
-                {
-                  return new charts.BarChart(
-                    dataList (snapshot.data),
-                    vertical: true,
-                    animate: true,
-                    barGroupingType: charts.BarGroupingType.grouped,
-                    behaviors: [new charts.SeriesLegend(
-                      position: charts.BehaviorPosition.bottom,
-                      horizontalFirst: false,  //legend show vertically
-
-                    )],
-                    animationDuration: Duration(
-                      microseconds: 2000,
-                    ),
-                  );
-                }
-              }),
-        ),
+          SizedBox(height: 20),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text("RM",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.underline,
+                )),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height * 0.40,
+            child: FutureBuilder(
+                future: getData(),
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  if (snapshot.connectionState == ConnectionState.none ||
+                      snapshot.hasData == false) {
+                    return Center(child: CircularProgressIndicator());
+                  } else {
+                    return new charts.BarChart(
+                      dataList(snapshot.data),
+                      vertical: true,
+                      animate: true,
+                      barGroupingType: charts.BarGroupingType.grouped,
+                      behaviors: [
+                        new charts.SeriesLegend(
+                          position: charts.BehaviorPosition.bottom,
+                          horizontalFirst: false, //legend show vertically
+                        )
+                      ],
+                      animationDuration: Duration(
+                        microseconds: 2000,
+                      ),
+                    );
+                  }
+                }),
+          ),
+        ]),
       ),
     );
   }
@@ -83,42 +137,32 @@ class _DonateReportScreenState extends State<DonateReportScreen> {
         "https://yitengsze.com/a_gifhope/php/load_donationReport.php";
     final res = await http.get(urlLoadJobs);
 
-      if (res.body.contains("nodata")) 
-      {
-        return false;
-      } 
-      
-      else 
-      {
-        Map<String, dynamic> map = json.decode(res.body);  //json decode will return dynamic
-        final List<dynamic> donateData = map["donate"].toList();
+    if (res.body.contains("nodata")) {
+      return false;
+    } else {
+      Map<String, dynamic> map =
+          json.decode(res.body); //json decode will return dynamic
+      final List<dynamic> donateData = map["donate"].toList();
 
-        return donateData;
-      }
+      return donateData;
     }
+  }
 }
 
-List<charts.Series<CDonate, String>> dataList(List<dynamic> apiData) 
-{
+List<charts.Series<CDonate, String>> dataList(List<dynamic> apiData) {
   List<CDonate> list = new List();
 
-  for (int i=0; i< apiData.length; i++)
-  {
-  
-    list.add(new CDonate (apiData[i]['genre'], apiData[i]['donate']));
+  for (int i = 0; i < apiData.length; i++) {
+    list.add(new CDonate(apiData[i]['genre'], apiData[i]['donate']));
   }
 
   return [
-        
-
-        new charts.Series<CDonate, String>(
-          id: 'Collected Donation',
-          data: list,
-          domainFn: (CDonate donate, _) => donate.genre,
-          measureFn: (CDonate donate, _) => int.parse(donate.donate),
-          colorFn: (CDonate donate, _) => charts.MaterialPalette.red.shadeDefault
-        ),
-
-      ]; 
-  
+    new charts.Series<CDonate, String>(
+        id: 'Collected Donation',
+        data: list,
+        domainFn: (CDonate donate, _) => donate.genre,
+        measureFn: (CDonate donate, _) => int.parse(donate.donate),
+        colorFn: (CDonate donate, _) =>
+            charts.MaterialPalette.red.shadeDefault),
+  ];
 }
