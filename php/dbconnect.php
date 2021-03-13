3deba7712a50c8281d0a7d5cfb8f12b2867e2846
Wynn <wynnylimweiyi@gmail.com>
2021-03-13 16:43:18 +0800

    First php

---
+++ php/dbconnect.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost";
+$username   = "yitengsz_weiyi0106";
+$password   = "Wynnylimweiyi0106";
+$dbname     = "yitengsz_weiyi"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname); 
+if ($conn->connect_error) {    
+    die("Connection failed: " . $conn->connect_error); 
+}
+?> 
